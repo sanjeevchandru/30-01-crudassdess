b815974cead3ffcd54265efdfb2e022d1539dbcf
sanjeevchandru <chandrusanjeev5@gmail.com>
2024-01-30 12:04:21 +0530

    30-01-crudassdess

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Assdess } from './Assdess/Assdess';
+
+function App() {
+  return (
+    <div className="App">
+      <Assdess/>
+    </div>
+  );
+}
+
+export default App;
